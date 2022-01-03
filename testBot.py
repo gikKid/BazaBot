b76972414fa0fc58967783c4c547983ef4d43645
Gandalf329 <72461796+Gandalf329@users.noreply.github.com>
2022-01-03 12:43:23 +0300

    Update testBot.py

--- testBot.py
+++ testBot.py
@@ -11,7 +11,6 @@ class_answer = ""
 number_of_course_answer = ""
 
 
-
 bot = telebot.TeleBot('5095964171:AAH0en9UsoV5YU0uR1mSYoGpUKMQOwshUW8')
 @bot.message_handler(commands=['start'])  
 def start_command(message):
