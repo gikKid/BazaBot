464b1bb7f3b05c91f38e84a357b752e8947e7ec1
agrich-8 <83089491+agrich-8@users.noreply.github.com>
2022-01-04 17:12:05 +0300

    comment

--- SearchForQuestions.py
+++ SearchForQuestions.py
@@ -4,40 +4,40 @@ import json
 import io
 
 
-def iter_unique_cells(row):
-    """Generate cells in *row* skipping empty grid cells."""
-    prior_tc = None
-    for cell in row.cells:
-        this_tc = cell._tc
-        if this_tc is prior_tc:
-            continue
-        prior_tc = this_tc
-        yield cell
+# def iter_unique_cells(row):
+#     """Generate cells in *row* skipping empty grid cells."""
+#     prior_tc = None
+#     for cell in row.cells:
+#         this_tc = cell._tc
+#         if this_tc is prior_tc:
+#             continue
+#         prior_tc = this_tc
+#         yield cell
 
-q=[]
-a=[]
-
-doc = docx.Document('PandasTableExtraction.docx')
-for table in doc.tables:
-    for row in table.rows:
-        for cell in iter_unique_cells(row):
-            for paragraph in cell.paragraphs:
-                pass
-
-table = doc.tables[0]
-
+# q=[]
+# a=[]
 
 # doc = docx.Document('PandasTableExtraction.docx')
-# table = doc.tables[0]
-# data = []
-# keys = None
-# for i, row in enumerate(table.rows):
-#     text = (cell.text for cell in row.cells)
+# for table in doc.tables:
+#     for row in table.rows:
+#         for cell in iter_unique_cells(row):
+#             for paragraph in cell.paragraphs:
+#                 pass
 
-#     if i == 0:
-#         keys = tuple(text)
-#         continue
-#     row_data = dict(zip(keys, text))
-#     data.append(row_data)
-# with io.open("table.json", "w", encoding="utf-8") as data_file:
-#     json.dump(data, data_file, indent=4, ensure_ascii=False)
+# table = doc.tables[0]
+
+
+doc = docx.Document('PandasTableExtraction.docx')
+table = doc.tables[0]
+data = []
+keys = None
+for i, row in enumerate(table.rows):
+    text = (cell.text for cell in row.cells)
+
+    if i == 0:
+        keys = tuple(text)
+        continue
+    row_data = dict(zip(keys, text))
+    data.append(row_data)
+with io.open("table.json", "w", encoding="utf-8") as data_file:
+    json.dump(data, data_file, indent=4, ensure_ascii=False)
